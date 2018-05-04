6b4364631b29f50eee13b3a3863e5689691f74f8
Harry P <harrypeach1999@gmail.com>
2018-05-04 22:57:30 +0100

    Changed theme selector to a dropdown

--- html/assets/js/modal.js
+++ html/assets/js/modal.js
@@ -1,18 +1,19 @@
 var modal = document.getElementById('modal');
 var btn = document.getElementById("modal-settings-icon");
 var span = document.getElementsByClassName("close")[0];
+var theme_selector = document.getElementById("theme-select");
 
 btn.onclick = function () {
     modal.className = "modal show";
     modal.style.display = "block";
 }
 
-// span.onclick = function () {
-//     modal.className = "modal hide";
-// }
-
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.className = "modal hide";
     }
+}
+
+theme_selector.onchange = function () {
+    document.getElementById('theme').href='assets/css/overlays/' + theme_selector.value + '.css'
 }

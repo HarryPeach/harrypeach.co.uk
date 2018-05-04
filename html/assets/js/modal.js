19377a5fd8ec407b5f28c2b3048f3701f07508ab
Harry Peach <harrypeach1999@gmail.com>
2018-05-04 18:25:03 +0100

    Added a settings modal and theme changes

---
+++ html/assets/js/modal.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById('modal');
+var btn = document.getElementById("modal-settings-icon");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}

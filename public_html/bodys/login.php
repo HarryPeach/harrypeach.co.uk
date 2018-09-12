ead23f1f9cb6c9ec182815f1175d8157f14be329
Harry Peach <harrypeach1999@gmail.com>
2018-09-12 18:22:27 +0100

    Clean wipe for Caspian rebuild

--- public_html/bodys/login.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "Thank you " . $_POST["username"] . " for attempting to log in. <br>";
-echo "Unfortunately our systems are harder than our abs <br>";
-echo $_POST["password"] . " is a weeny baby password";

96ae9bbcaaeb7f9a282935c052c1bdbe158124cf
Harry Peach <harrypeach1999@gmail.com>
2020-04-30 17:01:06 +0100

    Implemented Skeleton correctly

--- components/Skeleton.tsx
+++ components/Skeleton.tsx
@@ -5,13 +5,23 @@ type SkeletonProps = {
 	className?: string;
 	width?: number;
 	height?: number;
+	visible: boolean;
 };
 
-export default function Skeleton({ width, height, className }: SkeletonProps) {
+export default function Skeleton({
+	width,
+	height,
+	className,
+	visible,
+}: SkeletonProps) {
 	return (
 		<div
 			className={clsx(styles.skeleton, className)}
-			style={{ width: width, height: height }}
+			style={{
+				width: width,
+				height: height,
+				display: visible ? "block" : "none",
+			}}
 		></div>
 	);
 }
--- pages/projects/[project].tsx
+++ pages/projects/[project].tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import Router from "next/router";
 
 import * as Variants from "../../src/js/animationVariants";
 import projects from "../../data/projects.json";
 import Card from "../../components/Card";
+import Skeleton from "../../components/Skeleton";
 
 import styles from "./project.module.css";
 
@@ -21,10 +22,17 @@ type ProjectProps = {
 
 export default function Project({ project }: ProjectProps) {
 	const [imageLoaded, setImageLoaded] = React.useState(false);
+	const imageRef = React.useRef(null);
 
 	const goBack = () => {
 		Router.back();
 	};
+
+	useEffect(() => {
+		if (imageRef.current?.complete) {
+			setImageLoaded(true);
+		}
+	});
 
 	return (
 		<>
@@ -36,7 +44,11 @@ export default function Project({ project }: ProjectProps) {
 					>
 						<h1 className={styles.title}>{project.title}</h1>
 						<h2 className={styles.subtitle}>{project.desc}</h2>
-						{/* <Skeleton className={styles.skeleton} height={300} /> */}
+						<Skeleton
+							className={styles.skeleton}
+							height={300}
+							visible={!imageLoaded}
+						/>
 						<picture className={styles.coverImage}>
 							<source
 								srcSet={project.coverImage + ".webp"}
@@ -53,7 +65,11 @@ export default function Project({ project }: ProjectProps) {
 							<img
 								src={project.coverImage + ".jpg"}
 								alt="Cover Photo"
+								style={{
+									display: imageLoaded ? "block" : "none",
+								}}
 								onLoad={() => setImageLoaded(true)}
+								ref={imageRef}
 							></img>
 						</picture>
 						<p className={styles.content}>{project.descFull}</p>

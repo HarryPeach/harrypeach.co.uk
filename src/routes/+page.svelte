0269d34574ffeb5bc444648d9fca57a33350c2df
Harry Peach <harrypeach1999@gmail.com>
2024-04-17 20:18:25 +0100

    feat: add marquee, basic fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script>
-    import Marquee from "svelte-fast-marquee";
+	import Marquee from 'svelte-fast-marquee';
+    import UnderConstruction from "$lib/assets/under_construction.gif";
 </script>
-<h1>Harry Peach</h1>
-<Marquee>This is a marquee!</Marquee>
+
+<div class="container">
+	<h1 class="title">Harry Peach</h1>
+	<Marquee>welcome to my epic website!!</Marquee>
+    <img src={UnderConstruction} alt="Under Construction">
+</div>
+
+<style>
+	.title {
+		font-size: 4em;
+		font-weight: 900;
+	}
+
+	.container {
+		margin-top: 10%;
+	}
+</style>

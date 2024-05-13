7c71c750edc9a3829e718e0834a4e4d16f9cbd8d
Harry Peach <harrypeach1999@gmail.com>
2024-05-13 23:44:12 +0100

    feat: add profile image (Compressed)

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,13 @@
 <script>
 	import Window from '$lib/Window.svelte';
+    import Profile from "$lib/assets/profile.jpg";
 </script>
 
 <div class="container">
 	<Window title={'About'} content_title={"About Me"} selected={1}>
 		<div class="about-content">
 			<div class="image">
-				<img src={"https://via.assets.so/img.jpg?w=300&h=400&tc=blue&bg=#cecece&t=Profile"} alt={"Placeholder profile image"} />
+				<img src={Profile} width="300px" alt={"Placeholder profile image"} />
 			</div>
 			<div class="content">
 				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo sapiente non corrupti quidem assumenda! Minus eum sapiente reiciendis aliquid id tempora! Nesciunt eius sunt voluptate deleniti corporis ut provident at.</p>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,16 +3,10 @@
 </script>
 
 <div class="container">
-	<Window title={'Contact Me'} content_title={"Contact Me"} selected={3}>
-	</Window>
+	<Window title={'Contact Me'} content_title={"Contact Me"} selected={3}></Window>
 </div>
 
 <style>
-	.title {
-		font-size: 4em;
-		font-weight: 900;
-	}
-
 	.container {
 		margin-top: 10%;
 	}

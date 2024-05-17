75b32b879ae8fcf9aef29825c1d77a68a8897200
Harry Peach <harrypeach1999@gmail.com>
2024-05-17 21:03:18 +0100

    feat: add json file for projects and update projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,59 +1,44 @@
 <script>
 	import Window from '$lib/Window.svelte';
+
+	import Projects from "$lib/assets/projects.json";
+
+	const vocational_projects = Projects.vocational;
+	const educational_projects = Projects.educational;
 </script>
 
 <div class="container">
 	<Window title={'Projects'} content_title={'Projects'} selected={2}>
 		<h2>Vocational</h2>
-		<div class="project">
-			<img src="https://picsum.photos/250/180" alt="A placeholder" />
-			<div class="content">
-				<h3>Example Project Title</h3>
-				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut maiores amet autem adipisci
-					id natus in illo accusantium. Quia suscipit labore ea cupiditate necessitatibus tempora
-					minus voluptatibus quibusdam? Molestiae, delectus!
-				</p>
-				<a href="#">Link Here</a>
+		{#each vocational_projects as project}
+			<div class="project">
+				<img src="https://picsum.photos/250/180" alt="A placeholder" />
+				<div class="content">
+					<h3>{project.title}</h3>
+					<p>
+						{project.desc}
+					</p>
+					{#each project.links as link}
+						<a href={link.url}>{link.name}</a>
+					{/each}
+				</div>
 			</div>
-		</div>
-		<div class="project">
-			<img src="https://picsum.photos/250/180" alt="A placeholder" />
-			<div class="content">
-				<h3>Example Project Title</h3>
-				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut maiores amet autem adipisci
-					id natus in illo accusantium. Quia suscipit labore ea cupiditate necessitatibus tempora
-					minus voluptatibus quibusdam? Molestiae, delectus!
-				</p>
-				<a href="#">Link Here</a>
+		{/each}
+		<h2>Educational</h2>
+		{#each Projects.educational as project}
+			<div class="project">
+				<img src="https://picsum.photos/250/180" alt="A placeholder" />
+				<div class="content">
+					<h3>{project.title}</h3>
+					<p>
+						{project.desc}
+					</p>
+					{#each project.links as link}
+						<a href={link.url}>{link.name}</a>
+					{/each}
+				</div>
 			</div>
-		</div>
-		<div class="project">
-			<img src="https://picsum.photos/250/180" alt="A placeholder" />
-			<div class="content">
-				<h3>Example Project Title</h3>
-				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut maiores amet autem adipisci
-					id natus in illo accusantium. Quia suscipit labore ea cupiditate necessitatibus tempora
-					minus voluptatibus quibusdam? Molestiae, delectus!
-				</p>
-				<a href="#">Link Here</a>
-			</div>
-		</div>
-		<h2>Academic</h2>
-		<div class="project">
-			<img src="https://picsum.photos/250/180" alt="A placeholder" />
-			<div class="content">
-				<h3>Example Project Title</h3>
-				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut maiores amet autem adipisci
-					id natus in illo accusantium. Quia suscipit labore ea cupiditate necessitatibus tempora
-					minus voluptatibus quibusdam? Molestiae, delectus!
-				</p>
-				<a href="#">Link Here</a>
-			</div>
-		</div>
+		{/each}
 	</Window>
 </div>
 

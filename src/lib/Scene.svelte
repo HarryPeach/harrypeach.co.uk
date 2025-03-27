2cc2a058e544afa00c6b9804e00d17d3f35a1809
Harry Peach <harrypeach1999@gmail.com>
2025-03-27 23:22:52 +0000

    chore: add peach glb

---
+++ src/lib/Scene.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+	import { T, useTask } from '@threlte/core';
+	import { interactivity } from '@threlte/extras';
+	import { Spring } from 'svelte/motion';
+    import { useLoader } from '@threlte/core'
+
+	interactivity();
+
+    const gltf = useLoader(GLTFLoader).load('/assets/peach.glb')
+	const scale = new Spring(1);
+
+	let rotation = 0;
+	useTask((delta) => {
+		rotation += delta;
+	});
+</script>
+
+<T.PerspectiveCamera
+	makeDefault
+	position={[10, 10, 10]}
+	oncreate={(ref) => {
+		ref.lookAt(0, 1, 0);
+	}}
+/>
+
+<T.Mesh
+	rotation.y={rotation}
+	position.y={1}
+	scale={scale.current}
+	onpointerenter={() => {
+		scale.target = 1.5;
+	}}
+	onpointerleave={() => {
+		scale.target = 1;
+	}}
+>
+	<T.BoxGeometry args={[1, 2, 1]} />
+	<T.MeshBasicMaterial color="hotpink" />
+</T.Mesh>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { Canvas } from '@threlte/core'
+  import Scene from "$lib/Scene.svelte"
+</script>
+
+<Canvas>
+  <Scene />
+</Canvas>
